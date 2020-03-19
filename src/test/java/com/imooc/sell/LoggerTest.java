ab29b994751c2887e6ada0651fa88c7e42e62f3b
zhe <zygithub@gmail.com>
2020-03-19 00:30:46 -0400

    first

---
+++ src/test/java/com/imooc/sell/LoggerTest.java
@@ -0,0 +1,29 @@
+package com.imooc.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author zhe
+ * @date 2020/3/18 14:49
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+//@Data
+public class LoggerTest {
+
+    @Test
+    public void test1() {
+        String name = "imooc";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("name: " + name + " ,password: " + password);
+        log.info("name: {}, password: {}", name, password);
+        log.error("error...");
+        log.warn("warn...");
+    }
+}
